c1a119933cfc05b16de9b84339fe547af06be13b
Gabriel Montalvo <Gabriel09199@gmail.com>
2019-04-17 02:07:26 -0500

    Filtros aeropuerto completado

--- aeropuerto/aeropuertos.js
+++ aeropuerto/aeropuertos.js
@@ -3,31 +3,19 @@ const aeropuertos = new Vue({
     el: '#aeropuertos',
     data: {
         titulo: 'Aeropuertos',
-        aeropuertos: []
+        aeropuertos: [],
+        buscarAeropuerto: ''
     },
 
     methods: {
-        buscar() {
-            const busqueda = document.getElementById('buscarAeropuerto');
-            const contenido = document.querySelector('#contenido')
-            const texto = busqueda.value.toLowerCase();
-            contenido.innerHTML = '';
-            for(let aeropuerto of this.aeropuertos)
-            {
-                let nombre = aeropuerto.nombre_aeropuerto;
-                if(nombre.indexOf(texto) !== -1)
-                {
-                    contenido.innerHTML += `    
-                    <tr>
-                        <td>${ aeropuerto.nombre_aeropuerto }</td>
-                        <td>${ aeropuerto.ciudad_pais }</td>
-                        <td>${ aeropuerto.estado }</td>
-                        <td>${ aeropuerto.telefono }</td>
-                    </tr>
+        verificarBusqueda(){
+            
+        }
+    },
 
-                    `
-                }
-            }
+    computed: {
+        searchAirport: function () {
+            return this.aeropuertos.filter((aeropuerto) => aeropuerto.nombre_aeropuerto.toLowerCase().includes(this.buscarAeropuerto.toLowerCase()));
         }
     },
 
