c1f502da2af0187d496574363d3ede824fd45672
Gabriel Montalvo <Gabriel09199@gmail.com>
2019-04-23 20:22:21 -0500

    Arreglando la ruta del put de aeropuertos

--- ufly/src/main/java/com/empresariales/ufly/controllers/AeropuertoController.java
+++ ufly/src/main/java/com/empresariales/ufly/controllers/AeropuertoController.java
@@ -51,7 +51,7 @@ public class AeropuertoController
 	            .orElseThrow(() -> new ResourceNotFoundException("Aeropuerto", "id_aeropuerto", id_aeropuerto));
 	}
 	
-	@PutMapping("/modificar/{id_aeropuerto}")
+	@PutMapping("/{id_aeropuerto}")
 	public Aeropuerto modificarUsuario(@PathVariable(value = "id_aeropuerto") Short id_aeropuerto, @Valid @RequestBody Aeropuerto aeropuertoDetalle)
 	{
 		Aeropuerto aero=  aeropuertoRepository.findById(id_aeropuerto)
