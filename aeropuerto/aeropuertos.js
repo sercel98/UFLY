8ceff56920a15169e4d6aab1f666105f14145dca
sercel98 <36521785+sercel98@users.noreply.github.com>
2019-04-26 12:03:29 -0500

    fltros de aeropuerto

--- aeropuerto/aeropuertos.js
+++ aeropuerto/aeropuertos.js
@@ -4,7 +4,7 @@ const aeropuertos = new Vue({
     data: {
         titulo: 'Aeropuertos',
         aeropuertos: [],
-        buscarAeropuerto: ''
+        buscado: ''
     },
 
     methods: {
@@ -15,7 +15,26 @@ const aeropuertos = new Vue({
 
     computed: {
         searchAirport: function () {
-            return this.aeropuertos.filter((aeropuerto) => aeropuerto.nombre_aeropuerto.toLowerCase().includes(this.buscarAeropuerto.toLowerCase()));
+            var nombre = document.getElementById('customRadioInline1').checked;
+            var ciudad = document.getElementById('customRadioInline2').checked;
+            var pais = document.getElementById('customRadioInline3').checked;
+            
+            if (nombre) 
+            {
+                return this.aeropuertos.filter((aeropuerto) => aeropuerto.nombre_aeropuerto.toLowerCase().includes(this.buscado.toLowerCase()));
+            }
+            else if(ciudad)
+            {
+                return this.aeropuertos.filter((aeropuerto) => aeropuerto.fkciudades.nombre_ciudad.toLowerCase().includes(this.buscado.toLowerCase()));
+            }
+            else if(pais)
+            {
+                return this.aeropuertos.filter((aeropuerto) => aeropuerto.fkciudades.fkpais.nombre_pais.toLowerCase().includes(this.buscado.toLowerCase()));
+            }
+            else
+            {
+                return this.aeropuertos.filter((aeropuerto) => aeropuerto.fkestados_aeropuerto.nombre_estado.toLowerCase().includes(this.buscado.toLowerCase()));
+            }
         }
     },
 
