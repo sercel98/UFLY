e85c2a11fb044c981f220f1b663236f21785ceec
sercel98 <36521785+sercel98@users.noreply.github.com>
2019-05-11 11:53:57 -0500

    realizando tripulantes

---
+++ tripulantes/tripulantes.js
@@ -0,0 +1,47 @@
+const tripulantes = new Vue({
+    el: "#tripulantes",
+    data: {
+
+        tripulantes: [],
+        buscado: ''
+
+    },
+    methods: {
+
+        actualizarTripulante: function (id) {
+
+            location.href = 'perfil_tripulante.html?' + id;
+        },
+
+    },
+    computed: {
+        searchTripulantes: function () {
+            var cedula = document.getElementById('customRadioInline1').checked;
+            var nombre = document.getElementById('customRadioInline2').checked;
+            
+            if (cedula) 
+            {
+                return this.tripulantes.filter((tripulante) => tripulante.cedula.toString().includes(this.buscado.toLowerCase()));
+            }
+            else if(nombre)
+            {   
+                return this.tripulantes.filter((tripulante) => (tripulante.primer_nombre.toLowerCase() + tripulante.segundo_nombre.toLowerCase() + tripulante.primer_apellido.toLowerCase() + tripulante.segundo_apellido.toLowerCase()).includes(this.buscado.replace(' ', '').replace(' ', '').replace(' ', '').toLowerCase()));
+            }
+            else
+            {
+                return this.tripulantes.filter((tripulante) => tripulante.estado.toLowerCase().includes(this.buscado.toLowerCase()));
+
+            }
+        }
+    },
+
+
+    mounted() {
+        
+        fetch('http://localhost:8080/rest/tripulantes', { mode: 'cors' })
+            .then(response => response.json())
+            .then(tripulantes => {
+                this.tripulantes = tripulantes;
+            });
+    },
+});
