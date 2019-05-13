83d5036594d63f06fe210d7707e55ceb660eadd2
sercel98 <36521785+sercel98@users.noreply.github.com>
2019-05-13 10:38:08 -0500

    salas corregido y header tripulantes

--- aeropuerto/perfil_aeropuerto.js
+++ aeropuerto/perfil_aeropuerto.js
@@ -8,7 +8,7 @@ var app = new Vue({
         ciudadAeropuerto: '',
         paisAeropuerto: '',
         direccionAeropuerto: '',
-        telefonoAeropuerto: '',
+        telefonoAeropuerto: '', 
         estadoAeropuerto: '',
         nuevaSala: '',
         nuevoEstadoSala: '',
@@ -62,8 +62,6 @@ var app = new Vue({
                 }
                 else
                 {
-                    toastr.options.onHidden = function() { location.href='aeropuertos.html';}
-                    toastr.options.timeOut = 1000;
                     toastr.success('Se ha actualizado el aeropuerto exitosamente: ' + response.nombre_aeropuerto)
                 }
             })
